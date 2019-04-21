ed2b801c822e09eb82c5d7109199ff763e7f4c0b
Red Folder <markbryantaylor@yahoo.co.uk>
2019-04-21 20:32:41 +0100

    Added approve B2C facade

--- ActivityTracker/Approve.cs
+++ ActivityTracker/Approve.cs
@@ -2,6 +2,7 @@ using System;
 using System.Linq;
 using System.Net.Http;
 using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
@@ -9,16 +10,16 @@ using Microsoft.Azure.WebJobs.Host;
 
 namespace ActivityTracker
 {
-    public static class GetPendingApprovals
+    public static class Approve
     {
         private readonly static HttpClient _httpClient = new HttpClient();
 
-        [FunctionName("GetPendingApprovals")]
-        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)]HttpRequestMessage req,
+        [FunctionName("Approve")]
+        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)]HttpRequestMessage req,
                                                             ClaimsPrincipal claimsPrincipal,
                                                             TraceWriter log)
         {
-            log.Info("Received request to GetPendingApprovals");
+            log.Info("Received request to Approve");
 
             if (!claimsPrincipal.Identity.IsAuthenticated)
             {
@@ -32,10 +33,13 @@ namespace ActivityTracker
             ClaimsIdentity identity = (claimsPrincipal as ClaimsPrincipal)?.Identity as ClaimsIdentity;
             var name = identity.Claims.FirstOrDefault(x => x.Type == "name");
 
-            var url = Environment.GetEnvironmentVariable("GetPendingApprovalsUrl", EnvironmentVariableTarget.Process);
+            var url = Environment.GetEnvironmentVariable("ApproveUrl", EnvironmentVariableTarget.Process);
+
+            var requestBody = await req.Content.ReadAsStringAsync();
+            var payload = new StringContent(requestBody, Encoding.UTF8, "application/json");
 
             log.Info($"Making request to {url} as {name}");
-            var response = await _httpClient.GetAsync(url);
+            var response = await _httpClient.PostAsync(url, payload);
 
             log.Info($"Received status code {response.StatusCode}");
 

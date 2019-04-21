0a63b38068df02f672ff3f86efbe605f5f22f677
Red Folder <markbryantaylor@yahoo.co.uk>
2019-04-21 08:51:31 +0100

    Changed authentication mode

--- ActivityTracker/GetPendingApprovals.cs
+++ ActivityTracker/GetPendingApprovals.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Net.Http;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using System.Web.Http.Results;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Host;
@@ -12,9 +14,24 @@ namespace ActivityTracker
         private readonly static HttpClient _httpClient = new HttpClient();
 
         [FunctionName("GetPendingApprovals")]
-        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.User, "get", Route = null)]HttpRequestMessage req, TraceWriter log)
+        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)]HttpRequestMessage req,
+                                                            ClaimsPrincipal claimsPrincipal,
+                                                            TraceWriter log)
         {
             log.Info("Received request to GetPendingApprovals");
+
+            log.Info("Are we authenticated");
+            if (!claimsPrincipal.Identity.IsAuthenticated)
+            {
+                log.Info("Not authenticated");
+            }
+
+            ClaimsIdentity identity = (claimsPrincipal as ClaimsPrincipal)?.Identity as ClaimsIdentity;
+            foreach (var claim in identity.Claims)
+            {
+                log.Info($"{claim.Type} = {claim.Value}");
+            }
+
             var url = Environment.GetEnvironmentVariable("GetPendingApprovalsUrl", EnvironmentVariableTarget.Process);
 
             log.Info($"Making request to {url}");
